6aef281fb19e6b97f796f199f22758c315e5fe9f
Andresssrr24 <andresrueda2406@gmail.com>
2025-09-18 20:20:54 -0500

    models done

--- backend/app/db/models/faces.py
+++ backend/app/db/models/faces.py
@@ -1,1 +1,10 @@
-#TODO: Implement faces model
+from app.db.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey
+
+class FaceEmbeddings(Base):
+    __tablename__ = "face_embeddings"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    image_id: Mapped[int] = mapped_column(ForeignKey("images.id"), nullable=False)
+    face_embedding: Mapped[str] = mapped_column(String, nullable=False)  
--- backend/app/db/models/processing_queue.py
+++ backend/app/db/models/processing_queue.py
@@ -1,4 +1,19 @@
 from app.db.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, Datetime, String, ForeignKey
+from datetime import datetime
 
-#TODO: Implement processing queue model
+class ProcessingQueue(Base):
+    __tablename__ = "processing_queue"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    image_id: Mapped[int] = mapped_column(ForeignKey("images.id"), nullable=False)
+    status: Mapped[str] = mapped_column(String(50), nullable=False)
+    retries_left: Mapped[int] = mapped_column(Integer, nullable=False, default=3)
+    attempts: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    priority: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    started_at: Mapped[Datetime] = mapped_column(Datetime, nullable=False)
+    completed_at: Mapped[Datetime] = mapped_column(Datetime, nullable=False)
+    last_attempt_at: Mapped[Datetime] = mapped_column(Datetime, nullable=False)
+    error_message: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[Datetime] = mapped_column(Datetime, nullable=False, default=datetime.now)
